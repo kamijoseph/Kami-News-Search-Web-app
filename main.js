44cd1fa108a3cb0710e64c34704b1e4f92726603
Kami Joseph <josephkiarie561@gmail.com>
2025-01-29 18:46:12 +0300

    i am alive

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ import API_KEY from "./leen.js";
 const apiKey = API_KEY;
 const blogContainer = document.getElementById("blog-container");
 
-function fetchRandomNews() {
+async function fetchRandomNews() {
     try{
-
+        const apiUrl = `
+        https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles
     }
     catch(error) {
         console.error(`Error fetching News ${error}`);
     }
     return []
 }
+
+(async ()=> {
+    try{
+        const articles = await fetchRandomNews();
+        displayBlog(articles)
+    }
+    catch(error) {
+        console.error(`Error fetching News ${error}`);
+    }
+})
