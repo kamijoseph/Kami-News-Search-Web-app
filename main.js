3659674adcf5844b671323dd580f2bf99456d4bc
Kami Joseph <josephkiarie561@gmail.com>
2025-01-29 18:06:24 +0300

    news api fetch

---
+++ main.js
@@ -0,0 +1,15 @@
+
+import API_KEY from "./leen.js";
+
+const apiKey = API_KEY;
+const blogContainer = document.getElementById("blog-container");
+
+function fetchRandomNews() {
+    try{
+
+    }
+    catch(error) {
+        console.error(`Error fetching News ${error}`);
+    }
+    return []
+}
